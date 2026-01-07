d93d23d57ff6deb8c7809034f4251968bba4854b
Mahmud Birecikli <94433869+mahmudbirecikli@users.noreply.github.com>
2026-01-07 14:43:00 +0300

    adding binding methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,34 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
 import { LoginComponent } from '../identity/login/login.component';
 import { RegisterComponent } from '../identity/register/register.component';
+import { NgClass, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, TestComponent, HeaderComponent, FooterComponent, MainLayoutComponent,
-    LoginComponent, RegisterComponent
-  ],
+    LoginComponent, RegisterComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  _id = 'txt5';
+  _value: number = 33;
   title = 'firsto Project';
+  msg: string = 'this is a message!';
+  _Msg() {
+    alert(this.msg);
+  }
+  isSuc: boolean = true;
+  _ngClass = {
+    "txt-success": this.isSuc,
+    "txt-danger": !this.isSuc
+  }
+  GetMessage() { alert("get message button was clicked!") }
+
+  valMessage: string = '';
+  changeVal(input: string) {
+    this.valMessage = input.toUpperCase();
+
+  }
 }
